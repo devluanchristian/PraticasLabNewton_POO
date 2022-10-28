728ce96dfc9bf453c19d90c0485b263fe11a2401
devluanchristian <luan_christian-r@hotmail.com>
2022-10-28 15:21:12 -0300

    em andamento ...

---
+++ PraticaLabNewton_Paiva-main/pratica06_poo/Exe02/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    private String nome;
+    private String email;
+    private String telefone;
+    
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+}
